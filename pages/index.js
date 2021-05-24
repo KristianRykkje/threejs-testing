19e9264909a59eb9dcb69e912b3fcb7b85b461da
KristianRykkje <kriryk@gmail.com>
2021-05-24 11:14:25 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 import ThreeTest from "@/components/ThreeTest/ThreeTest";
-import { useCallback, useState } from "react";
 
 export default function Index() {
-  const [clicked, setClicked] = useState(false);
-
-  const handleClicked = useCallback(() => {
-    setClicked(prev => !prev);
-    console.log("clicked", clicked);
-  }, []);
-
   return (
     <div style={{ height: "100vh" }}>
-      <ThreeTest handleClicked={handleClicked} />
+      <ThreeTest />
     </div>
   );
 }
